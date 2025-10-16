76714d4eb0df0bb9886b4393954bee455ecca9c1
baohaoliao <liaobaohao@gmail.com>
2025-10-16 15:45:31 +0200

    validate

--- data_process/answer_validate.py
+++ data_process/answer_validate.py
@@ -3,7 +3,6 @@
 # Filter out samples that cannot be verified.
 #######################################################################
 
-import re
 import os
 import chz
 from tqdm import tqdm
@@ -11,7 +10,7 @@ from tqdm import tqdm
 import datasets
 from math_verify.metric import math_metric
 from math_verify.errors import TimeoutException
-from math_verify.parser import LatexExtractionConfig
+from math_verify.parser import ExprExtractionConfig, LatexExtractionConfig
 
 
 @chz.chz
@@ -22,48 +21,22 @@ class CLIConfig:
     answer_key: str = "answer"
     solution_key: str = "generations"
 
+    # separator
+    separator: str = "</think>"
+
     # Save
     output_dir: str = "validated_dataset"
 
 
-def extract_boxed(text: str) -> str:
-    """
-    Extract the context of the last \\boxed{...} in the text.
-    """
-    boxed_strs = []
-    stack = []
-    for ichar in range(len(text)):
-        if text[ichar] == "{":
-            stack.append(ichar)
-        elif text[ichar] == "}":
-            if len(stack) == 0:
-                raise ValueError("Unmatched }")
-            last_open_start = stack.pop()
-            # check if start is preceded by \boxed
-            if text[:last_open_start].endswith("\\boxed"):
-                boxed_strs.append(text[last_open_start + 1 : ichar])
-    if len(boxed_strs) > 0:
-        return boxed_strs[-1]
-    else:
-        # maybe there's something like '\boxed 2' without curly braces
-        match = re.search(r"\\boxed\s+([a-zA-Z0-9]+)", text)
-        if match:
-            return match.group(1)
-        else:
-            return ""
-        
-
 def validate_one_sample(
-        sample: dict, 
-        answer_key: str = "answer", 
-        solution_key: str = "generations"
-    ) -> bool:
-    """ 
+    sample: dict, answer_key: str = "answer", solution_key: str = "generations"
+) -> bool:
+    """
     Validate whether a question has a verifiable answer with math_verify.
     """
     verify_func = math_metric(
-        gold_extraction_target=(LatexExtractionConfig()),
-        pred_extraction_target=(LatexExtractionConfig()),
+        gold_extraction_target=(LatexExtractionConfig(),),
+        pred_extraction_target=(ExprExtractionConfig(), LatexExtractionConfig()),
     )
 
     answer = "\\boxed{" + sample[answer_key] + "}"
@@ -75,20 +48,19 @@ def validate_one_sample(
     scores = []
     for solution in solutions:
         ret_score = 0.0
-        pred = extract_boxed(solution)
 
-        if not pred:
-            continue
+        # math_verify can't handle multiple \boxed{}
+        if cli_config.separator in solution:
+            solution = solution.split(cli_config.separator)[-1].strip()
 
-        pred = "\\boxed{" + pred + "}"
         try:
-            ret_score, _ = verify_func([answer], [pred])
+            ret_score, _ = verify_func([answer], [solution])
         except Exception as e:
             print(f"Error in verifying: {e}")
             ret_score = 0
         except TimeoutException:
             ret_score = 0
-        
+
         scores.append(ret_score)
 
     if any(scores):
@@ -98,7 +70,9 @@ def validate_one_sample(
 
 def main(cli_config):
     # Load dataset
-    if os.path.exists(cli_config.dataset_name) and os.path.isdir(cli_config.dataset_name):
+    if os.path.exists(cli_config.dataset_name) and os.path.isdir(
+        cli_config.dataset_name
+    ):
         samples = datasets.load_from_disk(cli_config.dataset_name)[cli_config.split]
     else:
         samples = datasets.load_dataset(cli_config.dataset_name, split=cli_config.split)
@@ -106,9 +80,9 @@ def main(cli_config):
     # Validate samples
     for sample in tqdm(samples, desc="Validating samples", total=len(samples)):
         sample["is_valid"] = validate_one_sample(
-            sample, 
-            answer_key=cli_config.answer_key, 
-            solution_key=cli_config.solution_key
+            sample,
+            answer_key=cli_config.answer_key,
+            solution_key=cli_config.solution_key,
         )
 
     # Save filtered dataset
