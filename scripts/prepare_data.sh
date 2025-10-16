917cd4db95cec3fa1dfd0b43b850eee936434ad4
baohaoliao <liaobaohao@gmail.com>
2025-10-16 15:31:04 +0200

    script

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python -m data_process.answer_validate \
-    --dataset_name "open-r1/OpenR1-Math-220k" \
-    --split "default" \
-    --answer_key "answer" \
-    --solution_key "generations" \
-    --output_dir "/data/chatgpt-training-slc-a100/data/baliao/hint/data/openr1_validated"
+    --dataset_name="open-r1/OpenR1-Math-220k" \
+    --split="default" \
+    --answer_key="answer" \
+    --solution_key="generations" \
+    --output_dir="/mnt/nushare2/data/baliao/hint/data/openr1_validated"
