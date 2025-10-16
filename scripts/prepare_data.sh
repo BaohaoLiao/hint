0060465bf28c17ece597fb9cf22e1da1fcda846d
baohaoliao <liaobaohao@gmail.com>
2025-10-16 15:36:35 +0200

    split

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,7 +2,7 @@
 
 python -m data_process.answer_validate \
     dataset_name="open-r1/OpenR1-Math-220k" \
-    split="default" \
+    split="train" \
     answer_key="answer" \
     solution_key="generations" \
     output_dir="/mnt/nushare2/data/baliao/hint/data/openr1_validated"
