b7183b8ff53c2a5e35d831c3be3dbc1677d3bb8b
baohaoliao <liaobaohao@gmail.com>
2025-10-27 13:01:44 +0100

    hint generation

--- scripts/hint_generation.sh
+++ scripts/hint_generation.sh
@@ -4,7 +4,7 @@ cd /data/chatgpt-training-slc-a100/data/baliao/hint/00_data_process/hint
 
 DATA="/mnt/nushare2/data/baliao/hint/data/openr1/sampling_iterative/filtered_data/Qwen3-4B-Instruct-2507_passk_0.json"
 MODEL="azure-chat-completions-gpt-5-nano-2025-08-07-sandbox"
-SAVE_DIR="/mnt/nushare2/data/baliao/hint/data/openr1/sampling_iterative/hint_data"
+SAVE_DIR="/mnt/nushare2/data/baliao/hint/data/openr1/sampling_iterative/hint_data/Qwen3-4B-Instruct-2507_passk_0"
 
 mkdir -p ${SAVE_DIR}
 
@@ -12,4 +12,4 @@ python -m data_process.hint_generation_parallel \
     --dataset-path ${DATA} \
     --model-name ${MODEL} \
     --output-dir ${SAVE_DIR} \
-    --checkpoint-interval 10
+    --checkpoint-interval 50
